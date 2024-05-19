b9a4681aeb761bf03085d4355a2a5a2c5a589809
KyawMinHtwe <104247470+kyawminht@users.noreply.github.com>
2024-05-19 15:58:44 +0630

    necessary migration

---
+++ database/migrations/2024_05_19_091956_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('make');
+            $table->string('model');
+            $table->integer('registeration_year');
+            $table->decimal('price');
+            $table->string('ímage');
+            $table->foreignIdFor(User::class)->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
---
+++ database/migrations/2024_05_19_092013_create_bids_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Car;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bids', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Car::class);
+            $table->decimal('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bids');
+    }
+};
